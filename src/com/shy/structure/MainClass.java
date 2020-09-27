20da4576e33513d5a45980302d577b334d528c69
shihaoyan <923463567@qq.com>
2020-09-27 12:52:37 +0800

    基础包结构

--- src/com/shy/structure/MainClass.java
+++ src/com/shy/structure/MainClass.java
@@ -3,7 +3,7 @@ package com.shy.structure;
 /**
  * @author 石皓岩
  * @createDate 2020/9/26 20:01
- * 描述:创建主类
+ * 描述:创建主类,本类用于测试所有结构
  */
 public class MainClass {
     public static void main(String[] args) {
